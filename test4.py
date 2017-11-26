2a3b5828d35b719d15b3a988c2c98b32db6fea27
OniityanH <907209562@qq.com>
2017-11-27 03:50:05 +1030

    found the error, which is the chrome's problem, need to use chrome driver to test the code.

--- test4.py
+++ test4.py
@@ -2,15 +2,15 @@ from selenium import webdriver
 import time
 
 def log_fb():
-    br = webdriver.Firefox()
+    chrome_path= r"C:\Users\Allen Zhu\Desktop\chromedriver_win32\chromedriver.exe"
+    br = webdriver.Chrome(chrome_path)
     br.get('https://www.facebook.com/')
-    time.sleep(40)
+    time.sleep(3)
     user = br.find_element_by_css_selector('#email')
     user.send_keys('woaibeiqi@outlook.com')
     password=br.find_element_by_css_selector('#pass')
-    password.send_keys('-')
+    password.send_keys('Zhuyifei19930417')
     log=br.find_element_by_css_selector('#u_0_5')
     log.click()
-    print("over")
 
 log_fb()

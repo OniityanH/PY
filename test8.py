b6e1295867e1f570af584d5be8e03ee98e004d7d
OniityanH <907209562@qq.com>
2018-01-07 18:04:45 +0800

    gg finished

--- test8.py
+++ test8.py
@@ -9,15 +9,25 @@ def display(max_page):
     while page < max_page:
         url = 'https://search.bilibili.com/all?keyword=' + str(required_search)
         source_code = requests.get(url)
-        print(source_code)
         text1 = source_code.text
         soup = BeautifulSoup(text1)
+        #print(soup)
         for link in soup.findAll('a', {'class': 'title'}):
             href = link.get('href')
             title = link.get('title')
-            print(title)
-            print(href)
-        page += 1
+            true_href = "http://"+href[2:]
+            print(str(page) + title)
+            print(true_href)
+            get_single_item(true_href)
+            page += 1
+
+def get_single_item(item_url):
+    source_code = requests.get(item_url)
+    text = source_code.text
+    soup = BeautifulSoup(text)
+    for items in soup.findAll('div', {'class': 'v_desc report-scroll-module report-wrap-module'}):
+        print('\t\t'+items.text)
+
 
 max_page = input("请输入最大页数：")
 display(int(max_page))
